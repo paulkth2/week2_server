34672847aba05e4a5961fb4ee57a0cd4350f878e
Paul Kim <paulkth2@kaist.ac.kr>
2019-01-08 13:42:40 +0900

    user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // [LOAD PACKAGES]
 var express     = require('express');
+var mongodb     = require('mongodb');
 var app         = express();
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
@@ -97,72 +98,7 @@ app.get('/upload', function(req, res){
   res.render('upload');
 })
 */
-//Registration
-app.post('/register',(request, response, next) => {
-   var post_data = request.body;
-   var plaint_password = post_data.password;
-   var hash_data = saltHashPassword(plaint_password);
 
-   var password = hash_data.passwordHash;
-   var salt = hash_data.salt;
-
-   var name = post_data.name;
-   var email = post_data.email;
-   var insertJson = {
-      'email' : email,
-      'password': password,
-      'salt' : salt,
-      'name' : name
-   }
-   var db = client.db('users');
-
-   // check exist email
-   db.collection('user')
-      .find({'email':email}).count(function(err, number){
-         if(number != 0) {
-            response.json('Email already exists');
-            console.log('Email already exists');
-         } else {
-            db.collection('user')
-               .insertOne(insertJson, function(error, res){
-                  response.json('Registration success');
-                  console.log('Registration success');
-               })
-         }
-   })
-});
-
-// login
-
-app.post('/login', (request, response, next) => {
-   var post_data = request.body;
-
-   var email = post_data.email;
-   var userPassword = post_data.password;
-
-   var db = client.db('users');
-   db.collection('user')
-      .find({'email':email}).count(function(err, number) {
-         if(number == 0) {
-            response.json('email not exists');
-            console.log('email not exists');
-         } else {
-            db.collection('user')
-               .findOne({'email':email}, function(error, user) {
-                  var salt = user.salt;
-                  var hashed_password = checkHashPassword(userPassword, salt).passwordHash;
-                  var encrypted_password = user.password;
-                  if(hashed_password == encrypted_password) {
-                     response.json('login success');
-                     console.log('login success');
-                  } else { // wrong password
-                     response.json('wrong password');
-                     console.log('wrong password');
-                  }
-               })
-         }
-      })
-});
 
 
 
@@ -196,7 +132,68 @@ app.get('/uploads/:upload', function (req, res){
 // DEFINE MODEL
 var User = require('./models/user');
 var Image = require('./models/image');
+var Login = require('./models/login');
 
+//Registration
+app.post('/register',(request, response, next) => {
+   var post_data = request.body;
+   var plaint_password = post_data.password;
+   var hash_data = saltHashPassword(plaint_password);
+
+   var password = hash_data.passwordHash;
+   var salt = hash_data.salt;
+
+   var name = post_data.name;
+   var email = post_data.email;
+   var insertJson = {
+      'email' : email,
+      'password': password,
+      'salt' : salt,
+      'name' : name
+   }
+
+   // check exist email
+   Login.find({'email':email}).count(function(err, number){
+         if(number != 0) {
+            response.json('Email already exists');
+            console.log('Email already exists');
+         } else {
+            Login.insertOne(insertJson, function(error, res){
+                  response.json('Registration success');
+                  console.log('Registration success');
+               })
+         }
+   })
+});
+
+// login
+
+app.post('/login', (request, response, next) => {
+   var post_data = request.body;
+
+   var email = post_data.email;
+   var userPassword = post_data.password;
+
+    Login.find({'email':email}).count(function(err, number) {
+         if(number == 0) {
+            response.json('email not exists');
+            console.log('email not exists');
+         } else {
+            Login.findOne({'email':email}, function(error, user) {
+                  var salt = user.salt;
+                  var hashed_password = checkHashPassword(userPassword, salt).passwordHash;
+                  var encrypted_password = user.password;
+                  if(hashed_password == encrypted_password) {
+                     response.json('login success');
+                     console.log('login success');
+                  } else { // wrong password
+                     response.json('wrong password');
+                     console.log('wrong password');
+                  }
+               })
+         }
+      })
+});
 // GET ALL BOOKS
 app.get('/api/users', function(req,res){
     User.find(function(err, users){
